66c55ee8f0e27db7a3e6c53aab94404fc0ce8d23
Ton Casals <157851694+Tontito05@users.noreply.github.com>
2024-10-14 11:34:39 +0200

    actual tremplate

--- src/Player.h
+++ src/Player.h
@@ -2,7 +2,9 @@
 
 #include "Entity.h"
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_render.h"
+#include "Box2D/Box2D.h"
+
+struct SDL_Texture;
 
 class Player : public Entity
 {
@@ -20,10 +22,23 @@ public:
 
 	bool CleanUp();
 
+	// L08 TODO 6: Define OnCollision function for the player. 
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+	void OnCollisionEnd(PhysBody* physA, PhysBody* physB);
+
 public:
 
 	//Declare player parameters
-	float speed = 0.2f;
-	std::string textureName; 
+	float speed = 5.0f;
 	SDL_Texture* texture = NULL;
+	int texW, texH;
+
+	//Audio fx
+	int pickCoinFxId;
+
+	// L08 TODO 5: Add physics to the player - declare a Physics body
+	PhysBody* pbody;
+	float jumpForce = 1.2f; // The force to apply when jumping
+	bool isJumping = false; // Flag to check if the player is currently jumping
 };
